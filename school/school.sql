7b847be42e565cd35efe78f9422a95e0b2b74163
dan-abend <dan.abend@kellogg.northwestern.edu>
2022-01-18 16:37:15 +0000

    completed school and crm

--- school/school.sql
+++ school/school.sql
@@ -11,9 +11,33 @@ CREATE TABLE students (
   first_name TEXT,
   last_name TEXT,
   email TEXT,
-  phone_number TEXT
+  phone_number INTEGER
 );
 
 -- Create the rest of the tables
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
 
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time INTEGER,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
+
+CREATE TABLE enrollments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  section_id INTEGER,
+  student_id INTEGER
+);
